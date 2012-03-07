e2165fde5182c559c0cce428cfd0fcfa3a738c48
Mikhail Krasilnikov <mihalych@vsepofigu.ru>
2012-03-07 11:12:56 +0400

    Добавлен класс NetworkException

---
+++ lib/CommonBundle/Exceptions/NetworkException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Исключительная ситуация при работе с сетью
+ *
+ * @copyright 2011, Михаил Красильников, <mihalych@vsepofigu.ru>
+ *
+ * @package CommonBundle
+ *
+ * @author Михаил Красильников <mihalych@vsepofigu.ru>
+ */
+
+namespace Eresus\CommonBundle\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Исключительная ситуация при работе с сетью
+ *
+ * @package CommonBundle
+ *
+ * @since 1.0
+ */
+class NetworkException extends RuntimeException
+// @codeCoverageIgnoreStart
+{
+}
+// @codeCoverageIgnoreEnd
